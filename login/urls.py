79e4b550f37dc881ebb3d7111ebe764af53deb90
Piyush Dive <piyushdive835@gmail.com>
2023-10-08 00:15:08 +0530

    Fixed Issue: #2

--- login/urls.py
+++ login/urls.py
@@ -5,6 +5,6 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
+    url(r'^login/$', auth_views.LoginView, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.LogoutView, {'template_name': 'logged_out.html'}, name='logout'),
 ]
